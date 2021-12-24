f9e72d42f03a4b0b0c6dc0bbf85bd64b1bd14c90
Ahmed abdulrazzaq <aabdulra19@bw.edu>
2021-12-24 11:46:42 -0500

    Without ContaxtAPI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
@@ -6,6 +7,9 @@ import FeedbackData from "./data/feedbackData";
 import FeedBackStatus from "./components/FeedbackStats";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/About";
+import AboutPageLink from "./components/AboutPageLink";
+
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
   const deleteFeedBack = (id) => {
@@ -18,14 +22,29 @@ function App() {
     setFeedback([newFeedback, ...feedback]);
   };
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedBack} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedBack}
+                />
+              </>
+            }
+          ></Route>
+
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <AboutPageLink />
       </div>
-    </>
+    </Router>
   );
 }
 
